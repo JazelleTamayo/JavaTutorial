cf047bb1674f0ea284cbe09ab2e554b35b12da66
JazelleTamayo <jazellet5@gmail.com>
2025-06-30 17:45:17 +0800

    Add files via upload

---
+++ Chapter1/SongLyrics.java
@@ -0,0 +1,8 @@
+public class SongLyrics {
+   public static void main(String[] args) {
+      System.out.println("Cause I'm not your princess, this ain't our fairytale,");
+      System.out.println("I'm gonna find someone someday who might actually treat me well,");
+      System.out.println("This is a big world, that was a samll town,");
+      System.out.println("There in my rear view mirror disappearing now,");
+   }
+}
