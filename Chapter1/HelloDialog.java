1f1548d52acd4b78aa1a8129fce54b56614b35b3
JazelleTamayo <jazellet5@gmail.com>
2025-06-30 16:57:48 +0800

    Add files via upload

---
+++ Chapter1/HelloDialog.java
@@ -0,0 +1,10 @@
+//Written by: Jazelle Tamayo
+//Written on: June, 30, 2025
+
+import javax.swing.JOptionPane;
+
+public class HelloDialog {
+   public static void main(String[] args) {
+      JOptionPane.showMessageDialog(null, "Hello Motherfucker!");
+   }
+}
