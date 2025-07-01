c27cc863eb894b62b5140113d925162436773461
JazelleTamayo <jazellet5@gmail.com>
2025-07-01 20:43:25 +0800

    Update IntegerDemoInteractive.java

--- Chapter2/IntegerDemoInteractive.java
+++ Chapter2/IntegerDemoInteractive.java
@@ -13,10 +13,14 @@ public class IntegerDemoInteractive {
       short aShort = input.nextShort();
       System.out.print("Please enter a long integer: ");
       long aLong = input.nextLong();
+      input.nextLine();
+      System.out.print("Please enter your name: ");
+      String name = input.nextLine();
       
       System.out.println("The int is " +anInt);
       System.out.println("The byte is " +aByte);
       System.out.println("The short is " +aShort);
       System.out.println("The long is " +aLong);
+      System.out.println("Your name is: " +name);
    }
 }
