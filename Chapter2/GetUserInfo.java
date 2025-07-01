d8450fe4bbcbc08f30211270cf2d93040f9c6327
JazelleTamayo <jazellet5@gmail.com>
2025-07-01 20:30:49 +0800

    Update GetUserInfo.java

--- Chapter2/GetUserInfo.java
+++ Chapter2/GetUserInfo.java
@@ -6,10 +6,12 @@ public class GetUserInfo {
    
       Scanner input = new Scanner(System.in);
    
-      System.out.print("Please enter your name: ");
-      String name = input.nextLine();
       System.out.print("Please enter your age: ");
       int age = input.nextInt();
+      //Do this if the first input is not nextLine()
+      input.nextLine();
+      System.out.print("Please enter your name: ");
+      String name = input.nextLine();
    
       System.out.println("Your name is " +name+ " and you are " +age+ " years old. ");
    }
