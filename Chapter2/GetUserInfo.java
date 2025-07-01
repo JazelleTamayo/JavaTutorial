fcdf882a082bb21ac17569f5a5c935f020c582b1
JazelleTamayo <jazellet5@gmail.com>
2025-07-01 20:22:50 +0800

    Add files via upload

---
+++ Chapter2/GetUserInfo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GetUserInfo {
+
+   public static void main(String[] args) {
+   
+      Scanner input = new Scanner(System.in);
+   
+      System.out.print("Please enter your name: ");
+      String name = input.nextLine();
+      System.out.print("Please enter your age: ");
+      int age = input.nextInt();
+   
+      System.out.println("Your name is " +name+ " and you are " +age+ " years old. ");
+   }
+   
+}
