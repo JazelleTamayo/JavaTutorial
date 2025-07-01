7a1e8938619b15d35c75fdf339daf5c2bbf5438c
JazelleTamayo <jazellet5@gmail.com>
2025-07-01 16:39:43 +0800

    Add files via upload

---
+++ Chapter1/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo {
+   public static void main(String[] args) {
+      System.out.println("Quote: 'Beauty is the quality which, next to money, is generally " +
+      "the most attractive to the worst kinds of men; and, therefore, it is likely to entail " +
+      " a great deal of trouble on the possessor'");
+      
+      System.out.println("Movie: 'The Tenant of Wildfell Hall'");
+      System.out.println("Character: Mrs.Peggy Maxwell");
+                         
+   }
+}
