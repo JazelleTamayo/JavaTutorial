fd5fdb93c3ef7bf1652af2660fc268e173c0aa25
JazelleTamayo <jazellet5@gmail.com>
2025-07-01 16:49:46 +0800

    Add files via upload

---
+++ Chapter1/Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+   public static void main(String[] args) {
+      System.out.println("Program comments are nonexecuting statements you add to a file for documentation.");
+      
+      //single line comment
+      /*multiple line comment*/
+      
+      /**This is javadoc comment
+      *
+      *
+      *comment ends*/
+   }
+}
