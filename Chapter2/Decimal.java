7acb7cded311610b644b31230e31662c636331a3
JazelleTamayo <jazellet5@gmail.com>
2025-07-02 18:01:09 +0800

    Add files via upload

---
+++ Chapter2/Decimal.java
@@ -0,0 +1,9 @@
+public class Decimal {
+   public static void main(String[] args) {
+      double num1 = 23.45677d;
+      float num2 = 12.50f;
+      
+      System.out.printf("The double is %.3f%n", num1);
+      System.out.printf("The float is %.2f%n", num2);
+   }
+}
