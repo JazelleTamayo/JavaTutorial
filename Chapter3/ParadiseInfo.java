9784809ce65b36e854ee8893073354c758395627
JazelleTamayo <jazellet5@gmail.com>
2025-07-13 21:44:38 +0800

    Add files via upload

--- Chapter3/ParadiseInfo.java
+++ Chapter3/ParadiseInfo.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class ParadiseInfo {
    public static void main(String[] args) {
-      displayInfo();
+      Scanner input = new Scanner(System.in);
+      
+      System.out.print("Enter cutoff price for discount: ");
+      double price = input.nextDouble();
+      System.out.print("Enter discount rate as a whole number: ");
+      double discount = input.nextDouble();
+      
+      double savings = computerDiscountInfo(price, discount);
+      
+      System.out.println("Special this week on any service over " +price);
+      System.out.println("Discount of " +discount+ " percent");
+      System.out.println("That's a savings at least $" +savings);
+      
    }
    
-   public static void displayInfo() {
-      System.out.println("Paradise Day Spa wants to pamper you.");
-      System.out.println("We will make you look good.");
-      
+   public static double computerDiscountInfo(double price, double discount) {
+      double savings = price * discount / 100;
+      return savings;      
    }
 }
