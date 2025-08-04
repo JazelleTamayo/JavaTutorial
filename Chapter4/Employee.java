927898ceebadc9955a0633e5465ac5ef13c4b728
JazelleTamayo <jazellet5@gmail.com>
2025-08-04 21:27:29 +0800

    Add files via upload

---
+++ Chapter4/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+   private int empNum;
+   
+   public void setEmpNum(int emp) {
+      empNum = emp;
+   }
+   
+   public int getEmpNum() {
+      return empNum;
+   }
+   
+}
