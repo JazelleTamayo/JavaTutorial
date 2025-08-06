1b03875832d88bb5b923ac58cb739b52a0f42f57
JazelleTamayo <jazellet5@gmail.com>
2025-08-06 17:06:20 +0800

    Add files via upload

---
+++ Chapter4/Fruit.java
@@ -0,0 +1,29 @@
+public class Fruit {
+   String fruit;
+   double weight;
+   double price;
+   
+   public void setFruit(String aFruit) {
+      fruit = aFruit;
+   }
+   
+   public void setWeight(double aWeight) {
+      weight = aWeight;
+   }
+   
+   public void setPrice(double aPrice) {
+      price = aPrice;
+   }
+   
+   public String getFruit() {
+      return fruit;
+   }
+   
+   public double getWeight() {
+      return weight;
+   }
+   
+   public double getPrice() {
+      return price;
+   }
+}
