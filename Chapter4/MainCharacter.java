e535cab9e7560741b130060659aa10ebc6bf2cc0
JazelleTamayo <jazellet5@gmail.com>
2025-08-08 20:57:17 +0800

    Add files via upload

---
+++ Chapter4/MainCharacter.java
@@ -0,0 +1,11 @@
+
+
+public class MainCharacter {
+   public static void main(String[] args) {
+      Character c1 = new Character("Naruto", "Dattebayo!", 100, 50, 5);
+      c1.introduce();
+      c1.dialog();
+      c1.talkTo("Ejhie");
+      
+   }
+}
