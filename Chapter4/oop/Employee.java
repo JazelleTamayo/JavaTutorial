cb11ddad0850b1a130ed13b4ee39c92e60db8881
JazelleTamayo <jazellet5@gmail.com>
2025-08-16 18:05:29 +0800

    Add files via upload

---
+++ Chapter4/oop/Employee.java
@@ -0,0 +1,25 @@
+public class Employee {
+   private int empId;
+   private double empSal;
+   
+   public void setEmpNum(int id) {
+      empId = id;
+   
+   }
+   
+   public int getEmpNum() {
+      return empId;
+   
+   }
+   
+   public void setEmpSalary(double sal) {
+      empSal = sal;
+   
+   }
+   
+   public double getEmpSalary() {
+      return empSal;
+   
+   }
+
+}
